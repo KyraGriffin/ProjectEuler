e0aceed32b93a5a51fde98bc3c345daa0eae31d6
KyraGriffin <ke31grif@siena.edu>
2023-11-08 18:10:43 -0500

    Created problem 1 file

---
+++ problem_1.R
@@ -0,0 +1,30 @@
+## ---------------------------
+##
+## Script name: Multiples of 3 or 5 -- Problem 1 
+##
+## Purpose of script:If we list all the natural numbers below $10$ that are 
+##  multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+##  Find the sum of all the multiples of 3 or 5 below 1000.
+##
+## Author: Kyra Griffin-Mitchel
+##
+## Date Created: 2023-11-08
+##
+## Copyright (c) Kyra Griffin-Mitchell, 2023
+## Email: kyra.griffinmitchell@hmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
+
+## ---------------------------
+## load up the packages we will need:
+
+require(tidyverse)
+require(data.table)
+
+## ---------------------------
