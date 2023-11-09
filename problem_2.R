b3ecf041554da98fe0c18a1f78396ad29d018b48
KyraGriffin <ke31grif@siena.edu>
2023-11-09 12:22:56 -0500

    Created problem 2

---
+++ problem_2.R
@@ -0,0 +1,35 @@
+## ---------------------------
+##
+## Script name: Even Fibonacci Numbers -- Problem 2
+##
+## Purpose of script: Each new term in the Fibonacci sequence is generated 
+## by adding the previous two terms. By starting with 1 and 2, 
+## the first 10 terms will be:
+## 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,
+## By considering the terms in the Fibonacci sequence whose values do not exceed 
+## four million, find the sum of the even-valued terms.
+##
+## Author: Kyra Griffin-Mitchel
+##
+## Date Created: 2023-11-09
+##
+## Copyright (c) Kyra Griffin-Mitchell, 2023
+## Email: kyra.griffinmitchell@hmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
+
+## ---------------------------
+## load up the packages we will need:
+
+require(tidyverse)
+require(data.table)
+
+## ---------------------------
+
+
